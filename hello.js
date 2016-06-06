55468165af4faad926ba8593377e06b080104f75
Shahrukh Zindani <szindani@nectarom.com>
2016-06-06 05:25:23 -0500

    Add a text file

--- hello.js
+++ hello.js
@@ -1,5 +1,13 @@
-function upperCaser(input) {
-      input.toUpperCase();
-    }
-    
-    module.exports = upperCaser
+function getUserInput(firstname, lastname, gender, callback){
+	var fullname= firstname+ lastname;
+	if(typeof callback=== 'function'){
+
+		callback(fullname,gender);
+	}
+}
+
+function greetUser(customerName, gender){
+	console.log(customerName+' is a '+gender);
+}
+
+getUserInput('shahrukh', 'zindani', 'male', greetUser);
